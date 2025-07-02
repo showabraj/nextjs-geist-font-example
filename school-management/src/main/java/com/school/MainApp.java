4a3593a7023f97ed983946823411c39241299eb1
showabraj <sadiarose110@gmail.com>
2025-07-03 00:00:34 +0600

    Add school-management/src/main/java/com/school/MainApp.java 1751479234667

---
+++ school-management/src/main/java/com/school/MainApp.java
@@ -0,0 +1,22 @@
+package com.school;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+        primaryStage.setTitle("School Management System - Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
